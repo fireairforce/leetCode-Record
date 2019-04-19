0fd91761447dea03ff627ddfda8ed92bf8f6bfbd
zoomdong <1344492820@qq.com>
2019-04-19 16:22:33 +0800

    feat: add215

--- 215findKthLargest.js
+++ 215findKthLargest.js
@@ -3,7 +3,10 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function(nums, k) {
+var findKthLargest = function (nums, k) {
     // return nums.sort((a,b)=>b-a)[k-1]; 最简单的方法
-    
+    let len = arr.length - 1;
+    for (let i = len; i > len - k; i--) {
+       
+    }
 };
