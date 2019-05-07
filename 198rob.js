0e9a68dd2417bc19501a136a18f769ea43b5d5d5
zoomdong <1344492820@qq.com>
2019-05-07 22:46:44 +0800

    feat :finish 198

--- 198rob.js
+++ 198rob.js
@@ -2,9 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+var rob = function (nums) {
   let dp = [];
-  for(let i = 0 ;i<nums.length-1;i++){
-      
-  }  
+  if (nums.length <= 1) {
+    return nums.length === 0 ? 0 : nums[0];
+  }
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < nums.length ; i++) {
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  // console.log(dp)
+  return dp[nums.length-1]
 };
+console.log(rob([1, 2, 3, 1]));
