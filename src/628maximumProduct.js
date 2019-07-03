5afa2676987585fc6ceaef6336312cbaa07b87c2
fireairforce <osx@osxs-iMac.local>
2019-07-03 14:25:22 +0800

    feat:finish leetcode 628

--- src/628maximumProduct.js
+++ src/628maximumProduct.js
@@ -2,9 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maximumProduct = function(nums) {
-  let len = nums.length;
-  for (let i = 0; i < len; i++) {
-      
+let quickSort = arr => {
+  if (arr.length <= 1) {
+    return arr;
   }
+  let left = [];
+  let right = [];
+  let privotIndex = Math.floor(arr.length / 2);
+  let privot = arr.splice(privotIndex, 1)[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < privot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([privot], quickSort(right));
 };
+var maximumProduct = function(nums) {
+  if (nums.length < 3) {
+    return;
+  }
+  let newArr = quickSort(nums);
+  return Math.max(
+    newArr[newArr.length - 1] * newArr[newArr.length - 2] * newArr[newArr.length - 3],
+    newArr[0] * newArr[1] * newArr[newArr.length - 1]
+  );
+};
+// console.log("maximumProduct: ", maximumProduct([1,2,3,4]));
