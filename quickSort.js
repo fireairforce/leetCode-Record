2aab1574ebb8f9ec67d8aec6760fb1f46286feba
wduong1 <wudong1@xiaomi.com>
2019-07-17 11:33:17 +0800

    feat:fix lodash version

--- quickSort.js
+++ quickSort.js
@@ -1,18 +1,18 @@
-function quickSort(arr) {
-    if(arr.length<=1) {
+function quickSort(arr){
+    if(arr.length<=1){
         return arr;
     }
     let left = [];
     let right = [];
-    let privotIndex = Math.floor(arr.length / 2);
-    let privot = arr.splice(privotIndex, 1)[0];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < privot) {
+    let privotIndex = Math.floor(arr.length/2);
+    let privot = arr.splice(privotIndex,1)[0];
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<privot){
             left.push(arr[i]);
         } else {
             right.push(arr[i]);
         }
     }
-    return quickSort(left).concat([privot], quickSort(right));
+    return quickSort(left).concat([privot],quickSort(right));
 }
-// quickSort([1, 2, 3, 4, 1])
+console.log(quickSort([5,1,2,4,5,45,4,1]));
