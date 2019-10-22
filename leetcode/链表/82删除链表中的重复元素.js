b4fd0a03736385f865c146d56b0b7ae082532cdd
fireairforce <1344492820@qq.com>
2019-10-22 09:22:56 +0800

    feat:完成删除排序链表中的重复元素

---
+++ leetcode/链表/82删除链表中的重复元素.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(!head || !head.next) {
+        return head;
+    }
+    if(head.val === head.next.val && head.next) {
+        while(head.next && head.val === head.next.val) {
+            head = head.next;
+        }
+        return deleteDuplicates(head.next);
+    } else {
+        head.next = deleteDuplicates(head.next);
+    }
+    return head;
+};
