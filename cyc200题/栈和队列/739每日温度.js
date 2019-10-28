86df7f1e354acf7e6f250bb79613885207116af7
fireairforce <1344492820@qq.com>
2019-10-28 12:55:34 +0800

    feat:新增每日温度

--- cyc200题/栈和队列/739每日温度.js
+++ cyc200题/栈和队列/739每日温度.js
@@ -3,5 +3,19 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(T) {
-    
+  let num = [];
+  for (let i = 0; i < T.length; i++) {
+    let flag = 0;
+    for (let j = i; j < T.length; j++) {
+      if (T[j] > T[i]) {
+        num[i] = j - i;
+        flag = 1;
+        break;
+      }
+    }
+    if (!flag) {
+      num[i] = 0;
+    }
+  }
+  return num;
 };
