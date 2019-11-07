31c3b6b623f19cb64dc3442ab6656f09d87ab30f
fireairforce <1344492820@qq.com>
2019-11-07 23:37:23 +0800

    feat:新增字符串的全排列

--- ByteDance/算法题/字符串的排列.js
+++ ByteDance/算法题/字符串的排列.js
@@ -4,5 +4,43 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-    
+    if(s1.length > s2.length){
+        return false;
+    }
+    let S = fullpermutate(s1);
+    // console.log(s2);
+    for(let i = 0;i<S.length;i++){
+        if(s2.indexOf(S[i])>-1){
+            return true;
+        }
+    }
+    return false;
 };
+
+// 对字符串进行全排列
+var fullpermutate = (str) => {
+  const result = [];
+  if (str.length > 1) {
+    // 遍历每个字符
+    for (let i = 0; i < str.length; i++) {
+      // 拿到当前元素
+      let left = str[i];
+      //   拿到除了当前元素的其他元素组合体
+      let rest = str.slice(0, i) + str.slice(i + 1, str.length);
+      //   获取到其他元素的全排列体
+      let preResult = fullpermutate(rest);
+      for (let j = 0; j < preResult.length; j++) {
+        let temp = left + preResult[j];
+        result.push(temp);
+      }
+    }
+  } else if (str.length === 1) {
+    result.push(str);
+  }
+  return result;
+};
+
+console.log(checkInclusion("ab","eidboaoo"));
+// console.log(fullpermutate('ab'));
+// "ab"
+// "eidboaoo"
