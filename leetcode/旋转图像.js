46476478b2d1ac60774cfd0c15ca87e4475a4a87
fireairforce <1344492820@qq.com>
2019-11-15 11:54:41 +0800

    feat:新增旋转图像

--- leetcode/旋转图像.js
+++ leetcode/旋转图像.js
@@ -3,5 +3,19 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    
+  let len = matrix.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i; j < len; j++) {
+      if (i !== j) {
+        let temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+  }
+  for(let i = 0;i<matrix.length;i++){
+      matrix[i] = matrix[i].reverse();
+  }
+  return matrix;
 };
+
