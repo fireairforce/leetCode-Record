5161574fdf5fa86d884aac752a3b6ed56e023d22
fireairforce <1344492820@qq.com>
2019-11-19 09:03:54 +0800

    feat:新增排序算法

--- javascript-case/实现bind函数.js
+++ javascript-case/实现bind函数.js
@@ -1,17 +1,16 @@
-Function.prototype.mybind = function (context) {
-    if (typeof this !== 'function') {
-        throw new TypeError('Error');
+Function.prototype.mybind = function(context) {
+  if (typeof this !== "function") {
+    throw new TypeError("Error");
+  }
+  // 更换this的指向
+  let _this = this;
+  let arg = [...arguments].slice(1);
+  return function F() {
+    // 处理函数使用 new 的情况
+    if (this instanceof F) {
+      return new _this(...arg, ...arguments);
+    } else {
+      return _this.apply(context, arg.concat(...arguments));
     }
-    // 更换this的指向
-    let _this = this;
-    let arg = [...arguments].slice(1);
-    return function F() {
-        // 处理函数使用 new 的情况
-        if (this instanceof F) {
-            return new _this(...arg, ...arguments);
-        } else {
-            return _this.apply(context, arg.concat(...arguments));
-        }
-    }
-}
-
+  };
+};
