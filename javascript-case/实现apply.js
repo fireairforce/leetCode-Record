ca81546ff782aa59ef897e29421bc2d8629f5e86
fireairforce <1344492820@qq.com>
2019-11-20 16:55:20 +0800

    feat:手写js原理题目

--- javascript-case/实现apply.js
+++ javascript-case/实现apply.js
@@ -1,19 +1,33 @@
-Function.prototype.myapply = function (context) {
-   if(typeof this !== 'function'){
-       throw new TypeError('not function');
-   }
-   // 如果传递的执行上下文为null，就将环境切换为window    
-   context = context || window;
-   // 然后在context对象上新建一个属性,是一个函数    
-   context.fn = this;
-   //    这个地方是取参数
-   let result;
-   if(arguments[1]){
-       result = context.fn(...arguments[1]);
-   } else {
-       result = context.fn();
-   }
-  //  删除了这个属性 　　    
-   delete context.fn;
-   return result;
-}
+Function.prototype.myapply = function(context) {
+  if (typeof this !== "function") {
+    throw new TypeError("not function");
+  }
+  // 如果传递的执行上下文为null，就将环境切换为window
+  context = context || window;
+  // 然后在context对象上新建一个属性,是一个函数
+  context.fn = this;
+  //    这个地方是取参数
+  let result;
+  if (arguments[1]) {
+    result = context.fn(...arguments[1]);
+  } else {
+    result = context.fn();
+  }
+  //  删除了这个属性
+  delete context.fn;
+  return result;
+};
+
+Function.prototype.Myapply = function(context = window, args) {
+  if (typeof this !== "function") {
+    throw new TypeError("不是函数");
+  }
+  context = context || window;
+  context.fn = this;
+  if (Array.isArray(args)) {
+    result = context.fn(...args);
+  } else {
+    result = context.fn();
+  }
+  return result;
+};
--- javascript-case/实现bind函数.js
+++ javascript-case/实现bind函数.js
@@ -14,3 +14,17 @@ Function.prototype.mybind = function(context) {
     }
   };
 };
+
+Function.prototype.bind2 = function(context, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError("TypeError Wrong");
+  }
+  const _this = this;
+  return function F(...args1) {
+    if (this instanceof F) {
+      return new _this(...args, ...args1);
+    } else {
+      return _this.apply(context, arg.concat(args1));
+    }
+  };
+};
