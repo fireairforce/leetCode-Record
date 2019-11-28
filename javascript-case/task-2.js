c21391e5482b013498e4e04f578ae1cd6b9c989d
fireairforce <1344492820@qq.com>
2019-11-28 15:41:07 +0800

    feat:新增task

--- javascript-case/task-2.js
+++ javascript-case/task-2.js
@@ -1,20 +1,9 @@
-async function async1() {
-  console.log("async1 start");
-  await async2();
-  console.log("async1 end");
-}
-
-async function async2() {
-  console.log("async2");
-}
-
 console.log("script start");
 
 setTimeout(function() {
   console.log("setTimeout");
 }, 0);
 
-async1();
 
 new Promise(function(resolve) {
   console.log("promise1");
