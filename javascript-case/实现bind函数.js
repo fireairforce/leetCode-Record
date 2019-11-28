2c4ebd96fa2118009fe2737dd298b498e05902d3
fireairforce <1344492820@qq.com>
2019-11-28 14:49:41 +0800

    feat:练手

--- javascript-case/实现bind函数.js
+++ javascript-case/实现bind函数.js
@@ -1,20 +1,3 @@
-Function.prototype.mybind = function(context) {
-  if (typeof this !== "function") {
-    throw new TypeError("Error");
-  }
-  // 更换this的指向
-  let _this = this;
-  let arg = [...arguments].slice(1);
-  return function F() {
-    // 处理函数使用 new 的情况
-    if (this instanceof F) {
-      return new _this(...arg, ...arguments);
-    } else {
-      return _this.apply(context, arg.concat(...arguments));
-    }
-  };
-};
-
 Function.prototype.bind2 = function(context, ...args) {
   if (typeof this !== "function") {
     throw new TypeError("TypeError Wrong");
