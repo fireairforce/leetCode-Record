1af9baeab65d9a54ac7a32af4c233b1090f2db71
fireairforce <1344492820@qq.com>
2019-12-28 20:04:34 +0800

    feat:新增单词拆分

--- leetcode/139单词拆分.js
+++ leetcode/139单词拆分.js
@@ -3,6 +3,17 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict) {
-    
+var wordBreak = function (s, wordDict) {
+  const dp = Array(s.length + 1);
+  dp[0] = true;
+  for (let i = 0; i < s.length + 1; i++) {
+    for (let word of wordDict) {
+      if (dp[i - word.length] && word.length <= i) {
+        if (s.substring(i - word.length, i) === word) {
+          dp[i] = true;
+        }
+      }
+    }
+  }
+  return dp[s.length] || false;
 };
