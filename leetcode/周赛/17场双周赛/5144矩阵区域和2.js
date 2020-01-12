295a01bbbb94b72c39bfdf3397d3ce4198e69b9c
fireairforce <1344492820@qq.com>
2020-01-12 15:19:03 +0800

    feat:新增5144 O(mn)写法

--- leetcode/周赛/17场双周赛/5144矩阵区域和2.js
+++ leetcode/周赛/17场双周赛/5144矩阵区域和2.js
@@ -9,25 +9,29 @@ var matrixBlockSum = function(mat, K) {
     return;
   }
   let dp = [];
-  let answer = [];
   let n = mat[0].length;
   for (let i = 0; i <= m; i++) {
     dp[i] = new Array();
-    answer[i] = new Array();
     for (let j = 0; j <= n; j++) {
       dp[i][j] = 0;
-      answer[i][j] = 0;
     }
   }
   for (let i = 1; i <= m; i++) {
     for (let j = 1; j <= n; j++) {
-      dp[i][j] = mat[i-1][j-1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+      dp[i][j] =
+        mat[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
     }
   }
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-        // wait a minute
-    //    answer[i][j] = 
+  let answer = mat;
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      let x1 = Math.min(i + K, m);
+      let x2 = Math.max(i - K, 1);
+      let y1 = Math.min(j + K, n);
+      let y2 = Math.max(j - K, 1);
+      answer[i - 1][j - 1] =
+        dp[x1][y1] - dp[x1][y2 - 1] - dp[x2 - 1][y1] + dp[x2 - 1][y2 - 1];
     }
   }
+  return answer;
 };
