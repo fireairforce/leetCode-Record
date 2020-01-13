8f913bcb22ad16b4de83994de2bbbd7cda3cc471
fireairforce <1344492820@qq.com>
2020-01-13 14:06:00 +0800

    feat:新增5308或运算最小翻转次数(位运算相关)

--- leetcode/周赛/171周/5308或运算的最小翻转次数.js
+++ leetcode/周赛/171周/5308或运算的最小翻转次数.js
@@ -4,8 +4,28 @@
  * @param {number} c
  * @return {number}
  */
-var minFlips = function(a, b, c) {
-    
+var minFlips = function (a, b, c) {
+  let count = 0;
+  // 对a,b,c进行一个移位操作
+  while (a || b || c) {
+    let x = c & 1;
+    let y = b & 1;
+    let z = a & 1;
+    if (x===0) {
+      if (y!==0) {
+        count++;
+      }
+      if (z!==0) {
+        count++;
+      }
+    } else if (x===1) {
+      if (y === 0 && z === 0) {
+        count++;
+      }
+    }
+    a = a >> 1;
+    b = b >> 1;
+    c = c >> 1;
+  }
+  return count;
 };
-
-console.log(1|4);
