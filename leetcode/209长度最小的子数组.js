2fbdccb885a72e76384cfa0bb8978fc9bde34943
fireairforce <1344492820@qq.com>
2020-01-15 23:53:27 +0800

    feat:新增209长度最小子数组(滑动窗口解法)

--- leetcode/209长度最小的子数组.js
+++ leetcode/209长度最小的子数组.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var minSubArrayLen = function(s, nums) {
-  
+  let j = 0;
+  let sum = 0;
+  let len = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    while (sum >= s) {
+      //   len用来记录滑动窗口的最小值
+      if(len === 0 ) {
+          len = i - j + 1
+      } else {
+          len = Math.min(len, i - j + 1);
+      }
+      sum -= nums[j++];
+    }
+  }
+  return len;
 };
+// console.log(minSubArrayLen(7,[1,2,3,4,2,2]));
---
+++ leetcode/718最长重复子数组.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @return {number}
+ */
+var findLength = function(A, B) {
+
+};
