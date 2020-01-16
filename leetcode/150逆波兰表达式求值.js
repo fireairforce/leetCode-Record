c878eb0b028158b6154442ca8df4fdf5056fa521
fireairforce <1344492820@qq.com>
2020-01-17 00:49:33 +0800

    feat:新增逆波兰表达式求值

---
+++ leetcode/150逆波兰表达式求值.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+  let stack = [];
+  for (let i = 0; i < tokens.length; i++) {
+    let token = tokens[i];
+    //   判断字符是不是数字
+    if (!Number.isNaN(Number(token))) {
+      stack.push(token);
+    } else {
+      let a = Number(stack.pop());
+      let b = Number(stack.pop());
+      if (token === "*") {
+        stack.push(b * a);
+      } else if (token === "/") {
+        //   js的除法
+        stack.push((b / a) >> 0);
+      } else if (token === "+") {
+        stack.push(b + a);
+      } else if (token === "-") {
+        stack.push(b - a);
+      }
+    }
+  }
+  return stack.pop();
+};
