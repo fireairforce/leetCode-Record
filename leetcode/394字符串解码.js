1fce7959085415598b159f2e509cfcae3781c21b
fireairforce <1344492820@qq.com>
2020-01-17 18:46:04 +0800

    feat:新增最长回文子串(dp和双向拓展)

--- leetcode/394字符串解码.js
+++ leetcode/394字符串解码.js
@@ -6,29 +6,31 @@
 // s = "3[a2[c]]", 返回 "accaccacc".
 // s = "2[abc] 3[cd] ef", 返回 "abcabccdcdcdef".
 
+
+// 按照栈的思路去写，这题就比较好出了，有括号的地方都用栈
 var decodeString = function (s) {
   if (!s) {
     return s;
   }
-  let stack_multi = [],
-    stack_res = [],
-    res = '';
-  let multi = 0;
+  let res = '';
+  let stackRes = []
+  let stackNum = [];
+  let num = 0;
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === '[') {
-      stack_multi.push(multi);
-      stack_res.push(res.toString());
-      multi = 0;
+    if ('0' <= s[i] && s[i] <= '9') {
+      num = num * 10 + parseInt(s[i]);
+    } else if (s[i] === '[') {
+      stackRes.push(res);
+      stackNum.push(num);
+      num = 0;
       res = '';
     } else if (s[i] === ']') {
       let temp = '';
-      let cur_multi = stack_multi.pop();
-      for (let i = 0; i < cur_multi; i++) {
+      let tempNum = stackNum.pop();
+      for (let i = 0; i < tempNum; i++) {
         temp += res;
       }
-      res = stack_res.pop() + temp;
-    } else if ('0' <= s[i] && s[i] <= 9) {
-      multi = multi * 10 + parseInt(s[i]);
+      res = stackRes.pop() + temp;
     } else {
       res += s[i];
     }
