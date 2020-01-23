575c842bc154da3f009eb24ce77e4c319043dd62
fireairforce <1344492820@qq.com>
2020-01-23 18:04:28 +0800

    feat:新增正则表达式

--- leetcode/10正则表达式匹配.js
+++ leetcode/10正则表达式匹配.js
@@ -3,8 +3,16 @@
  * @param {string} p
  * @return {boolean}
  */
+
+// 递归写法
 var isMatch = function(s, p) {
-  let re = new RegExp(p);
-  return re.test(p);
+  if (!p.length) {
+    return !s.length;
+  }
+  let first_match = s.length && (s[0] === p[0] || p[0] === ".");
+  if (p.length >= 2 && p[1] === "*") {
+    return isMatch(s, p.slice(2)) || (first_match && isMatch(s.slice(1), p));
+  } else return first_match && isMatch(s.slice(1), p.slice(1));
 };
-console.log(isMatch('a','aa'));
+
+// console.log(isMatch("aa", "a"));
