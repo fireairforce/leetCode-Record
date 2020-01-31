d9c2b2b236fb5bf3b23d582e3cc1f19f691fde3a
fireairforce <1344492820@qq.com>
2020-01-31 20:45:22 +0800

    feat:205同构字符串

--- cyc200/字符串/205同构字符串.js
+++ cyc200/字符串/205同构字符串.js
@@ -4,5 +4,16 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
- 
+  let hash1 = {};
+  if (s.length !== t.length) {
+    return false;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i]) !== t.indexOf(t[i])) {
+      return false;
+    }
+  }
+  return true;
 };
+
+// console.log(isIsomorphic('aba','baa'));
---
+++ leetcode/331验证二叉树的前序序列化.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} preorder
+ * @return {boolean}
+ */
+var isValidSerialization = function(preorder) {
+  let str = preorder.split(",");
+  let len = str.length;
+  
+};
+
+const judge = (root,left,right) => {
+    
+}
