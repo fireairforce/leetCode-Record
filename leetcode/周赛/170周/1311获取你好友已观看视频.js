aeea07867249d3454ecb9f469d57b320c639866a
fireairforce <1344492820@qq.com>
2020-02-02 00:06:43 +0800

    feat:新增170周周赛

--- leetcode/周赛/170周/1311获取你好友已观看视频.js
+++ leetcode/周赛/170周/1311获取你好友已观看视频.js
@@ -1,14 +1,30 @@
-/**
- * @param {string[][]} watchedVideos
- * @param {number[][]} friends
- * @param {number} id
- * @param {number} level
- * @return {string[]}
- */
-var watchedVideosByFriends = function(watchedVideos, friends, id, level) {
-  let temp = friends[id];
-  let hash = {};
-  for (let i = 0; i < level; i++) {
-      
+const watchedVideosByFriends = (watchedVideos, friends, id, level) => {
+  const visited = new Uint8Array(friends.length);
+  let queue = [id];
+  visited[id] = 1;
+  while (level--) {
+    const next = [];
+    for (const id of queue) {
+      for (const newId of friends[id]) {
+        if (visited[newId] === 0) {
+          next.push(newId);
+          visited[newId] = 1;
+        }
+      }
+    }
+    queue = next;
   }
+
+  const freq = new Map();
+  for (const id of queue) {
+    for (const video of watchedVideos[id]) {
+      freq.set(video, freq.has(video) ? freq.get(video) + 1 : 1);
+    }
+  }
+
+  return Array.from(freq.entries())
+    .sort((a, b) => {
+      return a[1] === b[1] ? (a[0] > b[0] ? 1 : -1) : a[1] - b[1];
+    })
+    .map((item) => item[0]);
 };
