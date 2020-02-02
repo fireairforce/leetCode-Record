3af33e23dad195872a0c573041ccc70c3384edce
fireairforce <1344492820@qq.com>
2020-02-02 22:49:44 +0800

    feat:新增完全二叉树的节点个数

---
+++ leetcode/222完全二叉树的节点个数.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    if(!root) {
+        return 0;
+    }
+    return countNodes(root.left) + countNodes(root.right) + 1;
+ };
+ 
