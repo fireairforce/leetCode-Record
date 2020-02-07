86b119a49f1d48c281693742d865e16878255b09
fireairforce <1344492820@qq.com>
2020-02-07 23:23:33 +0800

    feat:新增汇总区间(双指针用法)

--- leetcode/228汇总区间.js
+++ leetcode/228汇总区间.js
@@ -2,6 +2,28 @@
  * @param {number[]} nums
  * @return {string[]}
  */
+// 双指针思路
 var summaryRanges = function(nums) {
-    
-};
+  let i = 0
+  let res = []
+  let len = nums.length - 1
+  while (i <= len) {
+    //   开始的指针
+    let start = i
+    while (i <= len - 1 && nums[i] + 1 == nums[i + 1]) {
+      i++
+    }
+    // 相等的话则说明只有一个数字
+    if (start === i) {
+      res.push(nums[i].toString())
+    } else {
+      res.push(`${nums[start]}->${nums[i]}`)
+    }
+    i++
+  }
+  return res
+}
+// console.log(summaryRanges([0, 1, 2, 4, 5, 7]))
+
+// [0,1,2,4,5,7]
+// ["0->2","4->5","7"]
