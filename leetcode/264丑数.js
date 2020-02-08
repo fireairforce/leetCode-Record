c5df7f3d6bcdb3ff64cbf1df58cc06f24adb6a98
fireairforce <1344492820@qq.com>
2020-02-08 12:33:32 +0800

    feat:新增264丑数( 三指针+动态规划)

---
+++ leetcode/264丑数.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function(n) {
+  let dp = Array(n+1).fill(0)
+  dp[0] = 1
+  let l2 = 0
+  let l3 = 0
+  let l5 = 0
+  for (let i = 1; i < n; i++) {
+    dp[i] = Math.min(2 * dp[l2], 3 * dp[l3], 5 * dp[l5])
+    if (dp[i] >= 2 * dp[l2]) {
+      l2++
+    }
+    if (dp[i] >= 3 * dp[l3]) {
+      l3++
+    }
+    if (dp[i] >= 5 * dp[l5]) {
+      l5++
+    }
+  }
+  return dp[n-1]
+}
