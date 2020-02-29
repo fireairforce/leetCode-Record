2231d937beb15c65eb181986fd600b9e04585b1e
fireairforce <1344492820@qq.com>
2020-03-01 00:03:45 +0800

    feat:新增剑指

--- 剑指offer/面试题56 - I. 数组中数字出现的次数.js
+++ 剑指offer/面试题56 - I. 数组中数字出现的次数.js
@@ -3,5 +3,21 @@
  * @return {number[]}
  */
 var singleNumbers = function(nums) {
-    
+  let sum = 0
+  //   sum为两个不同值的异或值
+  for (let item of nums) {
+    sum ^= item
+  }
+  //   得到二进制1的最低位
+  let flag = -sum & sum
+  let result = [0, 0]
+  //  用flag两组去进行^
+  for (let item of nums) {
+    if ((flag & item) === 0) {
+      result[0] = result[0] ^ item
+    } else {
+      result[1] = result[1] ^ item
+    }
+  }
+  return result
 }
