4a73ec08282e31aff39ecde2925e92dcaac36802
fireairforce <1344492820@qq.com>
2020-04-05 00:40:22 +0800

    feat:新增双周赛第四题

--- leetcodeContest/23周双周赛/4.js
+++ leetcodeContest/23周双周赛/4.js
@@ -2,6 +2,18 @@
  * @param {number[]} satisfaction
  * @return {number}
  */
-var maxSatisfaction = function(satisfaction) {
-    
-};
+var maxSatisfaction = function (satisfaction) {
+  satisfaction.sort((a, b) => a - b)
+  let sum = 0
+  let len = satisfaction.length
+  for (let i = 0; i < len; i++) {
+    let temp = 0
+    for (let j = i; j < len; j++) {
+      temp += (j - i + 1) * satisfaction[j]
+    }
+    sum = Math.max(temp, sum)
+  }
+  return sum
+}
+
+console.log(maxSatisfaction([-1, -8, 0, 5, -9]))
