57933a197116e47cb9e014feb13946132b256315
fireairforce <1344492820@qq.com>
2020-04-05 21:17:21 +0800

    feat:周赛第四题

--- leetcodeContest/183周/4.js
+++ leetcodeContest/183周/4.js
@@ -2,7 +2,20 @@
  * @param {number[]} stoneValue
  * @return {string}
  */
-var stoneGameIII = function(a) {
-  let len  = a.length
-  
-};
+var stoneGameIII = function (a) {
+  let len = a.length
+  let dp = Array(len + 1).fill(0)
+  for (let i = len - 1; i >= 0; i--) {
+    let max = -Infinity,
+      sum = 0
+    for (let j = i; j < Math.min(i + 3, len); j++) {
+      sum += a[j]
+      let temp = sum - dp[j + 1]
+      if (temp > max) {
+        max = temp
+      }
+    }
+    dp[i] = max
+  }
+  return dp[0] === 0 ? 'Tie' : dp[0] < 0 ? 'Bob' : 'Alice'
+}
