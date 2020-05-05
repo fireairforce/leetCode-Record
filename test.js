4a10f36e47bd98e54fc51f61a28837b4b723a87b
fireairforce <1344492820@qq.com>
2020-05-05 18:25:35 +0800

    feat: add test

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-let n = 10n;
-console.log(n * 100n);
+const test = [{ a: 'test' }, { a: 'test1' }]
+
+const test2 = [
+  { type: 'symbol', text: 'IFamily', sourceFile: 'test/test.ts' },
+  { type: 'punctuation', text: '[' },
+  { type: 'punctuation', text: ']' },
+]
+// res = test.reduce((a1,a2)=> a1.a + a2.a)
+res = test2.reduce((a1, a2) => {
+  console.log(a1, a2)
+  return a1.text + a2.text
+})
+console.log(res)
