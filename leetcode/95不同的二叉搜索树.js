b6ed3f279df8ff70108b35adc9b8e4427d3698ae
fireairforce <1344492820@qq.com>
2020-05-12 13:21:27 +0800

    feat: 新增不同的二叉搜索树

--- leetcode/95不同的二叉搜索树.js
+++ leetcode/95不同的二叉搜索树.js
@@ -1,14 +1,33 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+function TreeNode(val) {
+  this.val = val
+  this.left = this.right = null
+}
 /**
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n) {
-    
-};
+var generateTrees = function (n) {
+  if (n === 0) {
+    return []
+  }
+  const generateTree = (start, end) => {
+    if (start > end) {
+      return [null]
+    }
+    let res = []
+    for (let i = start; i <= end; i++) {
+      let ls = generateTree(start, i - 1)
+      let rs = generateTree(i + 1, end)
+      for (let l of ls) {
+        for (let r of rs) {
+          node = new TreeNode(i)
+          node.left = l
+          node.right = r
+          res.push(node)
+        }
+      }
+      return res
+    }
+  }
+  return generateTree(1, n)
+}
