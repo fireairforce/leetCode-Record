f5a8ad214b1897383a03caee62272f7c65277e35
fireairforce <1344492820@qq.com>
2020-05-30 17:39:00 +0800

    feat: 最大子序和

--- Daily/May/53. 最大子序和.py
+++ Daily/May/53. 最大子序和.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-      if len(nums) == 0:
-          return 0
-      if len(nums) == 1:
-          return nums[0]
-      # 初始化dp数组，dp[i]为nums[i]为结尾的子数组的最大值
-      dp = nums[:]
-      res = dp[0]
+      dp[0] = nums[0]
       for i in range(1, len(nums)):
-          # 更新dp[i]
-          dp[i] = max(dp[i], dp[i] + dp[i - 1])
-          res = max(res, dp[i])
-      return res
+          dp[i] = max(nums[i], dp[i-1] + nums[i])
+      res = -1
+      for i in range(len(dp))
+        res = max(res, dp[i])
+    return res
