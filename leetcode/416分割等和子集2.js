39bc003b8eeae509c7b52b205835808655b41f56
fireairforce <1344492820@qq.com>
2020-06-01 12:54:40 +0800

    feat: add 分割等和子集

--- leetcode/416分割等和子集2.js
+++ leetcode/416分割等和子集2.js
@@ -4,11 +4,29 @@ const solve = (nums) => {
     return false
   }
   let dp = []
-  for (let i = 1; i <= nums.length; i++) {
+  // init
+  for (let i = 0; i < nums.length; i++) {
     dp[i] = []
-    for (let j = 0;j<sum/2;j++) {
-      dp[i][0] = true;
+    dp[i][0] = true
+    for (let j = 1; j <= sum / 2; j++) {
+      dp[i][j] = false
     }
-    for (let )
   }
+  dp[0][nums[0]] = true;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j <= sum / 2; j++) {
+      if (j >= nums[i]) {
+        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]
+      } else {
+        dp[i][j] = dp[i - 1][j]
+      }
+    }
+  }
+  return dp[nums.length- 1][sum / 2]
 }
+
+// console.log(solve([1, 2, 5]))
+// console.log(solve([1, 5, 11, 5]))
+// console.log(solve([1, 2, 3, 5]))
+
+
---
+++ leetcodeContest/191周/5426重新规划路线3.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var minReorder = function(n, arr) {
+  let map = new Map()
+  
+  return res;
+};
