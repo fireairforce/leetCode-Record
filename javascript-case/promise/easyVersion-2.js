fd97c58e366961407bb441715c1c1c4c3e51ebb6
zoomdong <1344492820@qq.com>
2020-06-23 09:34:44 +0800

    feat: add promise

--- javascript-case/promise/easyVersion-2.js
+++ javascript-case/promise/easyVersion-2.js
@@ -42,16 +42,64 @@ function MyPromise(executor) {
   executor(resolve, reject)
 }
 
-MyPromise.prototype.then = function (onFulfilled, onRejected) {
-  if (this.status === PENDING) {
-    this.onFulfilledCallbacks.push(onFulfilled)
-    this.onRejectedCallbacks.push(onRejected)
-  } else if (this.status === FUILFILLED) {
-    // fuilfilled -> executor onFuilfilled -> pass value
-    onFulfilled(this.value)
+// MyPromise.prototype.then = function (onFulfilled, onRejected) {
+//   if (this.status === PENDING) {
+//     this.onFulfilledCallbacks.push(onFulfilled)
+//     this.onRejectedCallbacks.push(onRejected)
+//   } else if (this.status === FUILFILLED) {
+//     // fuilfilled -> executor onFuilfilled -> pass value
+//     onFulfilled(this.value)
+//   } else {
+//     // rejected -> executor onRejected -> pass error
+//     onRejected(this.error)
+//   }
+//   return this
+// }
+
+// update .then to support list call
+MyPromise.prototype.then = function (onFuilfilled, onRejected) {
+  let bridgePromise
+  let self = this
+  if (self.status === PENDING) {
+    return (bridgePromise = new MyPromise((resolve, reject) => {
+      self.onFulfilledCallbacks.push((value) => {
+        try {
+          // get the result of .then
+          let x = onFuilfilled(value)
+          resolve(x)
+        } catch (e) {
+          reject(e)
+        }
+      })
+      self.onRejectedCallbacks.push((err) => {
+        try {
+          let x = onRejected(err)
+          resolve(x)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    }))
+  } else if (self.status === FUILFILLED) {
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      try {
+        // 状态变为成功，会有相应的 self.value
+        let x = onFulfilled(self.value);
+        // 暂时可以理解为 resolve(x)，后面具体实现中有拆解的过程
+        resolvePromise(bridgePromise, x, resolve, reject);
+      } catch (e) {
+        reject(e);
+      }
+    })
   } else {
-    // rejected -> executor onRejected -> pass error
-    onRejected(this.error)
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      try {
+        // 状态变为失败，会有相应的 self.error
+        let x = onRejected(self.error);
+        resolvePromise(bridgePromise, x, resolve, reject);
+      } catch (e) {
+        reject(e);
+      }
+    });
   }
-  return this
 }
