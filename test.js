bc12086b886b0214db47ccc34efc278c6c304a18
zoomdong <1344492820@qq.com>
2020-06-25 21:56:40 +0800

    update test case

--- test.js
+++ test.js
@@ -1,24 +1,8 @@
-data.forEach((item) => {
-  if (item.id === tempData.firstCategoryId) {
-    this.industryName = item.name
-    this.banEntryFlag = item.banEntry
-    this.needCertification = item.needCertification
-    item.children.forEach((obj) => {
-      if (obj.id === tempData.secondCategoryId) {
-        this.industryName += '/' + obj.name
-        this.needCertification = obj.needCertification
-      }
-    })
-  }
-})
+let a = {
+  'xs': 1,
+  'qs': 2
+}
 
-
-data.filter(item => item.id === tempData.firstCategoryId).forEach(item => {
-  this.industryName = item.name
-  this.banEntryFlag = item.banEntry
-  this.needCertification = item.needCertification
-  item.children.filter(obj => obj.id === tempData.secondCategoryId).forEach(obj => {
-    this.industryName += '/' + obj.name
-    this.needCertification = obj.needCertification
-  })
-})
+// a.forEach((item)=> {
+//   console.log(item);
+// })
