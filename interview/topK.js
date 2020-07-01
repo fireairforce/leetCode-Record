977b2545d4a8e2130312f55a72468c25b62b6c41
zoomdong <1344492820@qq.com>
2020-07-01 22:11:12 +0800

    update

--- interview/topK.js
+++ interview/topK.js
@@ -1,7 +1,6 @@
 // 找数组中最大的k个元素
 
 // 利用快排的的思路来做
-
 // 思路就是先找个中枢值，把数组分成大于中枢值和小于中枢值的两部分
 // 如果交换之后中枢值正好是从后面数的第k个，那么就返回中枢值
 // 后面的数字大于中枢值，按照同样的方式从后面找
@@ -15,6 +14,7 @@ const findKthLargest = (nums, k) => {
     let i = left;
     // 把数组以 a[left]的大小拆分成两部分
     // 一部分小于 a[left]，一部分大于 a[right]
+    // [left, i] < A[left], [i + 1, j] >= A[left]
     for (let j = left + 1; j <= right; j++) {
       if (nums[j] < nums[left]) {
         if (j !== ++i) {
