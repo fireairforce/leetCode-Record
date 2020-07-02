6618a42155b33b91eacd87f1660ebe0d96024f65
zoomdong <1344492820@qq.com>
2020-07-02 10:11:19 +0800

    feat: add 复原IP地址

--- interview/93. 复原IP地址.js
+++ interview/93. 复原IP地址.js
@@ -19,15 +19,21 @@ var restoreIpAddresses = function (s) {
       res.push(list.join('.'))
       return
     }
+    // 单独要首位的情况
     backtrak([...list, str[0]], str.slice(1))
-    if (str[0] !== 0 && str.length > 1) {
+    // 非单独，但需要首位不为0
+    if (str[0] !== '0' && str.length > 1) {
       backtrak([...list, str.slice(0, 2)], str.slice(2))
     }
     // 按照条件去进行一个回溯
-    if (str[0] !== 0 && parseInt(str.slice(0, 3)) <= 255 && str.length > 2) {
+    if (str[0] !== '0' && parseInt(str.slice(0, 3)) <= 255 && str.length > 2) {
       backtrak([...list, str.slice(0, 3)], str.slice(3))
     }
   }
   backtrak([], s)
   return res
 }
+
+
+// console.log(restoreIpAddresses("25525511135"));
+// console.log(restoreIpAddresses("010010"));
