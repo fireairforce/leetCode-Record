36609a9181759afbe66668eda94cdc65f6596d27
zoomdong <1344492820@qq.com>
2020-07-03 23:31:24 +0800

    update cdoe

--- javascript-case/sort/quickSort.js
+++ javascript-case/sort/quickSort.js
@@ -1,22 +1,4 @@
 const quickSort = (arr) => {
-  if (arr.length <= 0) {
-    return arr
-  }
-  let left = []
-  let right = []
-  let privotIndex = Math.floor(arr.length >> 1)
-  let privot = arr.splice(privotIndex, 1)[0]
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < privot) {
-      left.push(arr[i])
-    } else {
-      right.push(arr[i])
-    }
-  }
-  return quickSort(left).concat([privot], quickSort(right))
-}
-
-const quickSort = (arr) => {
   if (!arr.length) {
     return arr
   }
