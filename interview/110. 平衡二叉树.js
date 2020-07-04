e463c82ac2d2e95c24cd73c7915ddd2e42f330e9
zoomdong <1344492820@qq.com>
2020-07-04 23:37:24 +0800

    feat: 平衡二叉树

---
+++ interview/110. 平衡二叉树.js
@@ -0,0 +1,24 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  if (!root) {
+    return true;
+  }
+  return (
+    Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.right) && isBalanced(root.right)
+  )
+};
+
+const depth = (root) => {
+  if (!root) {
+    return 0
+  }
+  return 1 + Math.max(depth(root.left), depth(root.right))
+}
