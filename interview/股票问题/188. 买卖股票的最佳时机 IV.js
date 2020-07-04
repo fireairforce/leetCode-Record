68fcc569741152512afdd44f373fefab71ea9d0f
zoomdong <1344492820@qq.com>
2020-07-04 21:05:12 +0800

    feat: add 股票问题

--- interview/股票问题/188. 买卖股票的最佳时机 IV.js
+++ interview/股票问题/188. 买卖股票的最佳时机 IV.js
@@ -9,6 +9,22 @@ var maxProfit = function (k, prices) {
   if (len === 0) {
     return 0
   }
+  // k值比len / 2 大的话，就按照k是正无穷来做处理
+  if (k > len / 2) {
+    for (let i = 0; i < len; i++) {
+      dp[i] = Array(2).fill(0)
+    }
+    for (let i = 0; i < len; i++) {
+      if (i === 0) {
+        dp[i][0] = 0;
+        dp[i][1] = -prices[i]
+        continue
+      }
+      dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+      dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+    }
+    return dp[len - 1][0];
+  }
   for (let i = 0; i < len; i++) {
     dp[i] = []
     for (let k1 = 0; k1 <= k; k1++) {
@@ -26,7 +42,7 @@ var maxProfit = function (k, prices) {
         continue;
       }
       dp[i][k1][0] = Math.max(dp[i - 1][k1][0], dp[i - 1][k1][1] + prices[i])
-      dp[i][k1][1] = Math.max(dp[i - 1][k1][1], dp[i - 1][k1][0] - prices[i])
+      dp[i][k1][1] = Math.max(dp[i - 1][k1][1], dp[i - 1][k1 - 1][0] - prices[i])
     }
   }
   return dp[len - 1][k][0];
@@ -34,3 +50,4 @@ var maxProfit = function (k, prices) {
 
 // console.log(maxProfit(2,[2,4,1]));
 // console.log(maxProfit(2, [3,2,6,5,0,3]));
+// console.log(maxProfit(2, [3,3,5,0,0,3,1,4]));
--- interview/股票问题/README.md
+++ interview/股票问题/README.md
@@ -204,5 +204,30 @@ dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k - 1][0] - prices[i])
 k = any integer 
 当 k 为任意整数的时候，这个时候给 k 增加一个范围就可以了
 ```js
-
+ let dp = []
+  let len = prices.length;
+  if (len === 0) {
+    return 0
+  }
+  for (let i = 0; i < len; i++) {
+    dp[i] = []
+    for (let k1 = 0; k1 <= k; k1++) {
+      dp[i][k1] = []
+      for (let j = 0; j < 2; j++) {
+        dp[i][k1][j] = 0
+      }
+    }
+  }
+  for (let i = 0; i < len; i++) {
+    for (let k1 = k; k1 >= 1; k1--) {
+      if (i === 0) {
+        dp[i][k1][0] = 0;
+        dp[i][k1][1] = -prices[i]
+        continue;
+      }
+      dp[i][k1][0] = Math.max(dp[i - 1][k1][0], dp[i - 1][k1][1] + prices[i])
+      dp[i][k1][1] = Math.max(dp[i - 1][k1][1], dp[i - 1][k1 - 1][0] - prices[i])
+    }
+  }
+  return dp[len - 1][k][0];
 ```
