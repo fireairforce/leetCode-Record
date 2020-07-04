5c7878f005c600da108818c8521116b1b43509a3
zoomdong <1344492820@qq.com>
2020-07-04 21:53:49 +0800

    feat: add 最小栈

--- interview/155. 最小栈.js
+++ interview/155. 最小栈.js
@@ -1,37 +1,35 @@
+// 常数时间内获取到栈里面到最小元素
+// 开个栈每次放两个元素进去，一个是当前元素，一个是栈里面的最小元素
 let MinStack = function () {
-  this.min = Number.MAX_VALUE;
   this.stack = []
 }
 
 MinStack.prototype.push = function (x) {
-  let min = this.min;
-  if (x < this.min) {
-    this.min = x;
+  if (this.stack.length === 0) {
+    this.stack.push(x)
+    this.stack.push(x)
+  } else {
+    // temp 先取到目前栈里面到最小元素，这里做个比较就可以了哈
+    let temp = this.stack[this.stack.length - 1]
+    this.stack.push(x)
+    if (temp < x) {
+      this.stack.push(temp)
+    } else {
+      this.stack.push(x)
+    }
   }
-  this.stack.push(x - min)
 }
 
 MinStack.prototype.pop = function () {
-   let item = this.stack.pop()
-   let min = this.min;
-   if (item < 0) {
-     this.min = min - item;
-     return min;
-   }
-   return item + min;
+  this.stack.pop()
+  this.stack.pop()
 }
 
 MinStack.prototype.top = function () {
-  let item = this.stack[this.stack.length - 1]
-  let min = this.min
-
-  if (item < 0) {
-    return min;
-  }
-
-  return item + min;
+  // 注意这里是减2 😓
+  return this.stack[this.stack.length - 2];
 }
 
 MinStack.prototype.getMin = function () {
-  return this.min;
+  return this.stack[this.stack.length - 1]
 }
