984189de1c19ac25b0a5d1da5c40787b75b7008e
zoomdong <1344492820@qq.com>
2020-07-08 22:08:27 +0800

    feat: add test case

--- javascript-case/promise/all.js
+++ javascript-case/promise/all.js
@@ -1,46 +1,22 @@
-Promise.all = function(promises) {
-  return new Promise((resolve, reject) => {
-    let result = [];
-    let index = 0;
-    let len = promises.length;
-    if(len === 0){
-        resolve(result);
-        return;
-    }
-    for(let i = 0;i<len;i++){
-        Promise.resolve(promises[i]).then(data =>{
-            result[i] = data;
-            index ++;
-            if(index === len){
-                resolve(result);
-            }
-        }).catch(err=>{
-            reject(err);
-        })
-    }
-  });
-};
-
-
-Promise.all = function(promises) {
-    return new Promise((resolve,reject) => {
-        let len = promises.length;
+Promise.all = function (promises) {
+    return new Promise((resolve, reject) => {
         let result = [];
         let index = 0;
-        if(len === 0){
+        let len = promises.length;
+        if (len === 0) {
             resolve(result);
+            return;
         }
-        for(let i = 0;i<len;i++){
-            Promise.resolve(promises[i]).then(res=>{
-                result[i] = res;
-                index ++;
-                if(index === len) {
+        for (let i = 0; i < len; i++) {
+            Promise.resolve(promises[i]).then(data => {
+                result[i] = data;
+                index++;
+                if (index === len) {
                     resolve(result);
-                    return;
                 }
-            }).catch((err)=>{
-                reject(err)
+            }).catch(err => {
+                reject(err);
             })
         }
-    })
-}
+    });
+};
