f68466a0467cd61cb4293aa9d04009182f9dd79b
zoomdong <1344492820@qq.com>
2020-07-11 19:07:29 +0800

    feat: add revert num

--- test.js
+++ test.js
@@ -31,3 +31,30 @@ person1.foo3().call(person2)
 person1.foo4()()
 person1.foo4.call(person2)()
 person1.foo4().call(person2)
+
+
+function convertNum(number) {
+  // write code here
+  let str = String(number);
+  let prefix = str[0] === '-' ? '-' : '';
+  
+  // 如果有前缀
+  if (prefix) {
+    // 前缀去,然后翻转
+    let newStr = str.slice(1).split('').reverse();
+    let index = 0
+    while (newStr[index] && newStr[index] === '0') {
+       index ++;
+    }
+    newStr = newStr.join('').slice(index)
+    return prefix + newStr;
+  } else {
+     let newStr = str.split('').reverse();
+     let index = 0
+    while (newStr[index] && newStr[index] === '0') {
+       index ++;
+    }
+    newStr = newStr.join('').slice(index)
+    return newStr;
+  }
+}
