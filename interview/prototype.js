8c10e5d93a6995a45ed20ac55efabe42480e8ce3
zoomdong <1344492820@qq.com>
2020-07-12 17:02:07 +0800

    update

---
+++ interview/prototype.js
@@ -0,0 +1,8 @@
+Object.prototype.a = 'Object';
+Function.prototype.a = 'Function';
+function Person(){};        
+var child = new Person();
+
+console.log(Person.a);
+console.log(child.a);
+console.log(child.__proto__.__proto__.constructor.constructor.constructor); 
--- test.js
+++ test.js
@@ -1,60 +1,79 @@
-var name = 'window'
-var person1 = {
-  name: 'person1',
-  foo1: function () {
-    console.log(this.name)
-  },
-  foo2: () => console.log(this.name),
-  foo3: function () {
-    return function () {
-      console.log(this.name)
-    }
-  },
-  foo4: function () {
-    return () => {
-      console.log(this.name)
-    }
-  }
-}
-var person2 = { name: 'person2' }
+// var name = 'window'
+// var person1 = {
+//   name: 'person1',
+//   foo1: function () {
+//     console.log(this.name)
+//   },
+//   foo2: () => console.log(this.name),
+//   foo3: function () {
+//     return function () {
+//       console.log(this.name)
+//     }
+//   },
+//   foo4: function () {
+//     return () => {
+//       console.log(this.name)
+//     }
+//   }
+// }
+// var person2 = { name: 'person2' }
 
-person1.foo1() // person1
-person1.foo1.call(person2) // person2
+// person1.foo1() // person1
+// person1.foo1.call(person2) // person2
 
-person1.foo2() // window
-person1.foo2.call(person2) // window
+// person1.foo2() // window
+// person1.foo2.call(person2) // window
 
-person1.foo3()()
-person1.foo3.call(person2)()
-person1.foo3().call(person2)
+// person1.foo3()()
+// person1.foo3.call(person2)()
+// person1.foo3().call(person2)
 
-person1.foo4()()
-person1.foo4.call(person2)()
-person1.foo4().call(person2)
+// person1.foo4()()
+// person1.foo4.call(person2)()
+// person1.foo4().call(person2)
 
 
-function convertNum(number) {
-  // write code here
-  let str = String(number);
-  let prefix = str[0] === '-' ? '-' : '';
+// function convertNum(number) {
+//   // write code here
+//   let str = String(number);
+//   let prefix = str[0] === '-' ? '-' : '';
   
-  // 如果有前缀
-  if (prefix) {
-    // 前缀去,然后翻转
-    let newStr = str.slice(1).split('').reverse();
-    let index = 0
-    while (newStr[index] && newStr[index] === '0') {
-       index ++;
-    }
-    newStr = newStr.join('').slice(index)
-    return prefix + newStr;
-  } else {
-     let newStr = str.split('').reverse();
-     let index = 0
-    while (newStr[index] && newStr[index] === '0') {
-       index ++;
-    }
-    newStr = newStr.join('').slice(index)
-    return newStr;
-  }
+//   // 如果有前缀
+//   if (prefix) {
+//     // 前缀去,然后翻转
+//     let newStr = str.slice(1).split('').reverse();
+//     let index = 0
+//     while (newStr[index] && newStr[index] === '0') {
+//        index ++;
+//     }
+//     newStr = newStr.join('').slice(index)
+//     return prefix + newStr;
+//   } else {
+//      let newStr = str.split('').reverse();
+//      let index = 0
+//     while (newStr[index] && newStr[index] === '0') {
+//        index ++;
+//     }
+//     newStr = newStr.join('').slice(index)
+//     return newStr;
+//   }
+// }
+
+
+var a = 1;  
+function b() {  
+    a = 10;  
+    return;  
+    function a() {console.log('function')}  
+}  
+b();  
+console.log(a);
+
+var a = 1;
+function b(){
+    a = 10;
+    return;
 }
+b();
+console.log(a);
+
