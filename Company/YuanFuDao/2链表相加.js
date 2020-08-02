c460c808d0fac6a9134eef2241a9426fec6f4241
zoomdong <1344492820@qq.com>
2020-08-02 19:40:30 +0800

    adjust

--- Company/YuanFuDao/2链表相加.js
+++ Company/YuanFuDao/2链表相加.js
@@ -10,16 +10,15 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    const ro = [];
-    const add = (l1,l2,carry) => {
-        if(l1 || l2 || carry) {
-            const val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-            ro.push(val%10);
-            return add(l1?l1.next:nul,l2 ? l2.next : null,Math.floor(val/10));
-        }
+var addTwoNumbers = function (l1, l2) {
+  const ro = []
+  const add = (l1, l2, carry) => {
+    if (l1 || l2 || carry) {
+      const val = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry
+      ro.push(val % 10)
+      return add(l1 ? l1.next : nul, l2 ? l2.next : null, Math.floor(val / 10))
     }
-    add(l1,l2,0,[]);
-    return ro;
-};
-
+  }
+  add(l1, l2, 0, [])
+  return ro
+}
