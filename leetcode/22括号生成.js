fa250679804702e6351e492a8aac83ab3fec002c
zoomdong <1344492820@qq.com>
2020-08-09 15:46:08 +0800

    update 括号生成

--- leetcode/22括号生成.js
+++ leetcode/22括号生成.js
@@ -2,21 +2,22 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-  let res = "";
-  bfs(res, "", 0, 0, n);
-};
-// l统计(的数目,r统计)的数目
-const bfs = (res, str, l, r, n) => {
-  // 注意这里也要边界上限定r>l
-  if (l > n || r > n || r > l) {
-    return;
+var generateParenthesis = function (n) {
+  let res = ''
+  // l统计(的数目,r统计)的数目
+  const dfs = (res, str, l, r, n) => {
+    // 注意这里也要边界上限定r>l
+    if (l > n || r > n || r > l) {
+      return
+    }
+    if (l === n && r === n) {
+      res.push(str)
+      return
+    }
+    dfs(res, str + '(', l + 1, r, n)
+    dfs(res, str + ')', l, r + 1, n)
+    return
   }
-  if (l === n && r === n) {
-    res.push(str);
-    return;
-  }
-  bfs(res, str + "(", l + 1, r, n);
-  bfs(res, str + ")", l, r + 1, n);
-  return;
-};
+  dfs(res, '', 0, 0, n)
+  return res
+}
