b59a6745bbc3f0ea199ca5f45bd0ef06f2a34d59
zoomdong <1344492820@qq.com>
2020-09-18 17:51:12 +0800

    format

--- test.js
+++ test.js
@@ -1,101 +1,29 @@
-// var name = 'window'
-// var person1 = {
-//   name: 'person1',
-//   foo1: function () {
-//     console.log(this.name)
-//   },
-//   foo2: () => console.log(this.name),
-//   foo3: function () {
-//     return function () {
-//       console.log(this.name)
-//     }
-//   },
-//   foo4: function () {
-//     return () => {
-//       console.log(this.name)
-//     }
-//   }
-// }
-// var person2 = { name: 'person2' }
+// output is [[]]
+const solve = (arr) => {
+  let len = arr.length
+  const res = []
+  //
+  const dfs = (nums) => {
+    // 边界条件
+    if (nums.length === len) {
+      res.push(nums)
+      return
+    }
+    for (let i = 0; i < len; i++) {
+      if (!nums.includes(arr[i])) {
+        nums.push(arr[i])
+        dfs(nums)
+        nums.pop()
+      }
+    }
+  }
+  dfs([])
+  return res;
+}
 
-// person1.foo1() // person1
-// person1.foo1.call(person2) // person2
+console.log(solve([1, 2, 3, 4, 5]))
+// 1 2 3 4 5
 
-// person1.foo2() // window
-// person1.foo2.call(person2) // window
+// 1 2 3 5 4
 
-// person1.foo3()()
-// person1.foo3.call(person2)()
-// person1.foo3().call(person2)
-
-// person1.foo4()()
-// person1.foo4.call(person2)()
-// person1.foo4().call(person2)
-
-
-// function convertNum(number) {
-//   // write code here
-//   let str = String(number);
-//   let prefix = str[0] === '-' ? '-' : '';
-  
-//   // 如果有前缀
-//   if (prefix) {
-//     // 前缀去,然后翻转
-//     let newStr = str.slice(1).split('').reverse();
-//     let index = 0
-//     while (newStr[index] && newStr[index] === '0') {
-//        index ++;
-//     }
-//     newStr = newStr.join('').slice(index)
-//     return prefix + newStr;
-//   } else {
-//      let newStr = str.split('').reverse();
-//      let index = 0
-//     while (newStr[index] && newStr[index] === '0') {
-//        index ++;
-//     }
-//     newStr = newStr.join('').slice(index)
-//     return newStr;
-//   }
-// }
-
-
-// var a = 1;  
-// function b() {  
-//     a = 10;  
-//     return;  
-//     function a() {console.log('function')}  
-// }  
-// b();  
-// console.log(a);
-
-// var a = 1;
-// function b(){
-//     a = 10;
-//     return;
-// }
-// b();
-// console.log(a);
-
-
-// function Page () {
-//     return this.hosts;
-// }
-
-// Page.hosts = ['p1']
-// Page.prototype.hosts = ['p1']
-
-// const p1 = new Page()
-// const p2 = Page();
-// console.log(p1.hosts);
-// console.log(p2.hosts);
-
-var a = 20;
-
-(function() {
-  console.log(name);
-  console.log(a);
-
-  var name = 'wd'
-  let a = 2;
-}())
+//
