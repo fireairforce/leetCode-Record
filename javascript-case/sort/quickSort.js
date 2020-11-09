27943be716d268410821eae4245c3654fd48d6b4
zoomdong <1344492820@qq.com>
2020-11-09 22:59:45 +0800

    feat: 修改quickSort代码

--- javascript-case/sort/quickSort.js
+++ javascript-case/sort/quickSort.js
@@ -1,17 +1,28 @@
-const quickSort = (arr) => {
-  if (!arr.length) {
-    return arr
-  }
-  let left = []
-  let right = []
-  let privotIndex = Math.floor(arr.length / 2)
-  let privot = arr.splice(privotIndex, 1)[0]
-  for (let item of arr) {
-    if (item < privot) {
-      left.push(item)
-    } else {
-      right.push(item)
-    }
-  }
-  return quickSort(left).concat([privot], quickSort(right))
+const swap = (x, y) => {
+  x = x ^ y;
+  y = x ^ y;
+  x = x ^ y;
 }
+
+const solve = (arr) => {
+  let n = arr.length;
+  const quickSort = (arr, l, r) => {
+    if (l >= r) {
+      return;
+    }
+    let x = arr[Math.floor((l + r) >> 1)], i = l - 1, j = r + 1;
+    while (i < j) {
+      while (arr[++i] < x);
+      while (arr[--j] > x);
+      if (i < j) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+      }
+    }
+    quickSort(arr, l, j);
+    quickSort(arr, j + 1, r);
+  }
+  quickSort(arr, 0, n - 1);
+  console.log(arr);
+}
+
+solve([5, 4, 7, 1, 2]);
