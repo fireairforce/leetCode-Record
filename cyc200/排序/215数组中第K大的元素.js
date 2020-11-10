e51efa59cc3cca365874549f934123e6bbd41d32
zoomdong <1344492820@qq.com>
2020-11-10 18:20:25 +0800

    feat: update code

--- cyc200/排序/215数组中第K大的元素.js
+++ cyc200/排序/215数组中第K大的元素.js
@@ -5,34 +5,32 @@
  */
 
 var findKthLargest = function (nums, k) {
-  k = nums.length - k
+  k = nums.length - k + 1
   let l = 0,
     r = nums.length - 1
-  quickSort(nums, l, r, k)
-  console.log('quickSort(nums, l, r, k): ', quickSort(nums, l, r, k))
-}
-
-const quickSort = (nums, left, right, index) => {
-  if (left >= right) {
-    return nums[right]
-  }
-  let x = nums[Math.floor((left + right) >> 1)],
-    i = left - 1,
-    j = right + 1
-  while (i < j) {
-    while (nums[++i] < x);
-    while (nums[--j] > x);
-    if (i < j) {
-      ;[nums[i], nums[j]] = [nums[j], nums[i]]
+  let res = -1
+  const quickSort = (nums, left, right, index) => {
+    if (left >= right) {
+      res = nums[left];
+      return
+    }
+    let x = nums[Math.floor((left + right) >> 1)],
+      i = left - 1,
+      j = right + 1
+    while (i < j) {
+      while (nums[++i] < x);
+      while (nums[--j] > x);
+      if (i < j) {
+        ;[nums[i], nums[j]] = [nums[j], nums[i]]
+      }
+    }
+    let s1 = j - left + 1
+    if (index <= s1) {
+      quickSort(nums, left, j, index)
+    } else {
+      quickSort(nums, j + 1, right, index - s1)
     }
   }
-  let s1 = j - left + 1
-  if (index <= s1) {
-    quickSort(nums, left, j, index)
-  } else {
-    quickSort(nums, j + 1, right, index - s1)
-  }
+  quickSort(nums, l, r, k)
+  return res
 }
-
-// console.log(findKthLargest([1, 2, 3, 4], 1));
-// console.log(findKthLargest());
